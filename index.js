a2778d3ac901db291e667d645eb9d6017f0d9449
shanza <shanza.nemat@qbatch.com>
2021-08-11 15:37:24 +0500

    second new branch

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 <body>
 
 <p>This is a paragraph.</p>
-<p>This is another paragraph.</p>
-<h1> i have make changes in the master branch</h1>
-<h1> this in my heading in the branch </h1>
-<h1> this in my heading in the newbranch </h1>
-<p>new line added in new branch</p>
+
+<p> second new branch </p>
 </body>
 </html>
